48e76482ce3988117c08e8eea1bdfb52dfdf9003
Mica Swyers <micaswyers@gmail.com>
2017-06-20 21:43:35 -0700

    Add route for blog, GH, Instagram

--- app.py
+++ app.py
@@ -9,10 +9,21 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/blogging')
+def blogging():
+	return redirect('https://micapie.com')
+
+@app.route('/coding')
+def coding():
+	return redirect('https://github.com/micaswyers')
+
+@app.route('/oninstagram')
+def oninstagram():
+	return redirect('https://www.instagram.com/micaswyers')
+
 @app.route('/tweeting')
 def tweeting():
 	return redirect('https://twitter.com/micaswyers')
 
-@app.route('/blogging')
-def blogging():
-	return redirect('https://micapie.com')
+if __name__ == "__main__":
+	app.run(host='0.0.0.0')
